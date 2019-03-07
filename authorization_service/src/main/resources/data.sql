4d08b033941cd483e2112ebcf0aa785e285821f8
Dilanka Samarathunga <dilz_rulz@live.com>
2019-03-07 14:46:51 +0530

    oAuth integration 1.2

--- authorization_service/src/main/resources/data.sql
+++ authorization_service/src/main/resources/data.sql
@@ -1,7 +1,8 @@
 INSERT INTO authority  VALUES(1,'ROLE_OAUTH_ADMIN');
 INSERT INTO authority VALUES(2,'ROLE_RESOURCE_ADMIN');
 INSERT INTO authority VALUES(3,'ROLE_PRODUCT_ADMIN');
-INSERT INTO credentials VALUES(1,b'1','oauth_admin','$2a$10$BurTWIy5NTF9GJJH4magz.9Bd4bBurWYG8tmXxeQh1vs7r/wnCFG2','0');
+INSERT INTO credentials
+VALUES (1, b'1', 'oauth_admin', '$2a$04$NvWBu43hMg6Y3Fl4nHx5jeEWKIlTNX.OsvdPiiz.ziffUs00Bj2iS', '0');
 INSERT INTO credentials VALUES(2,b'1','resource_admin','$2a$10$BurTWIy5NTF9GJJH4magz.9Bd4bBurWYG8tmXxeQh1vs7r/wnCFG2','0');
 INSERT INTO credentials  VALUES(3,b'1','product_admin','$2a$10$BurTWIy5NTF9GJJH4magz.9Bd4bBurWYG8tmXxeQh1vs7r/wnCFG2','0');
 INSERT INTO credentials_authorities VALUE (1,1);
